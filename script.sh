74fc9330f6b26ea905e09e0fd0d3b7636acd248d
NONAME <nikadoreuli47@gmail.com>
2023-02-02 02:07:40 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,13 +5,15 @@ mount /dev/sda3 /mnt &&
 mount --mkdir /dev/sda1 /mnt/boot/efi &&
 swapon /dev/sda2 &&
 pacman -Syy archlinux-keyring figlet --noconfirm &&
-pacstrap /mnt alacritty base base-devel bluez efibootmgr firefox gdm git gnome-bluetooth gnome-settings-daemon grub htop intel-ucode linux linux-firmware linux-headers lxsession-gtk3 mpv nautilus neovim net-tools networkmanager ntfs-3g obs-studio openssh papirus-icon-theme pipewire pipewire-alsa pipewire-jack pipewire-media-session pipewire-pulse sxhkd xf86-video-intel &&
+pacstrap /mnt alacritty base base-devel bluez bluez-utils efibootmgr kitty firefox git grub htop intel-ucode linux linux-firmware linux-headers lxsession-gtk3 mpv nautilus neovim net-tools networkmanager ntfs-3g obs-studio openssh papirus-icon-theme pipewire pipewire-alsa pipewire-jack pipewire-media-session pipewire-pulse pulsemixer rofi ttc-iosevka unzip waybar xf86-video-intel &&
 arch-chroot /mnt grub-install /dev/sda &&
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg &&
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/Asia/Tbilisi /etc/localtime &&
 arch-chroot /mnt sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen &&
+# here
 arch-chroot /mnt sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub &&
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg &&
+# here
 arch-chroot /mnt locale-gen &&
 arch-chroot /mnt touch /etc/hostname &&
 arch-chroot /mnt echo archlinux > /etc/hostname &&
